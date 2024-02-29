c6c0e8e2f27dc303e6b582e1718e96a8527aa5f7
bpoongs <bpoongs@gmail.com>
2024-02-29 19:46:46 +0530

    JS-functions

---
+++ js-demos/ways-to-create-a-function/function-demo.js
@@ -0,0 +1,22 @@
+// Stage 1: here we will define the function
+// all functions defined here do not take any arguments and do not 
+        //return any value
+
+// first way of defining of function
+function display1(){
+    console.log("first way of function definition");
+}
+
+// second way of defining of function
+let display2 = function(){
+    console.log("second way of function definition");
+}
+
+// third way of defining of function - arrow functions
+// arrow functions were introduced in ES6 version of javascript
+let display3 = () => {
+    console.log("third way of function definition");
+}
+
+
+
