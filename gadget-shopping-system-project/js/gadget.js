a38992502f80a88d1dee0efff0d14c8110edb9e4
bpoongs <bpoongs@gmail.com>
2024-03-19 18:21:53 +0530

    fetch api

--- gadget-shopping-system-project/js/gadget.js
+++ gadget-shopping-system-project/js/gadget.js
@@ -1,21 +1,20 @@
 let allCartGadgets = [];
 let allGadgets = []; // we have moved this JS object array to db.json file as json object array
 
-function loadGadgets(){
-    // the purpose of this function is to fetch the json data from db.json file
-        //- to do this we need to use fetch API
-        //- whenever we perform any crud operation on db.json file use fetch API
-    // and convert to JS object array
-        //- to do this Json.parse() function
-    // and store it in allGadgets array
-    // and call this function in displayGadgets()
-}
-
-function displayGadgets(){
-    
-    // traverse through the allGadgets array and display them
-    let data = '';
-    for(let eachGadget of allGadgets){
+function loadGadgets() {
+  // the purpose of this function is to fetch the json data from db.json file
+  //- to do this we need to use fetch API
+  //- whenever we perform any crud operation on db.json file use fetch API
+  // and convert to JS object array
+  //- to do this Json.parse() function
+  // and store it in allGadgets array
+  fetch("http://localhost:3000/gadgets")
+    .then((res) => res.json())
+    .then((data1) => {
+      console.log(data1);
+      allGadgets = data1;
+      let data = "";
+      for (let eachGadget of allGadgets) {
         data += `<div class='flex-item'>
                     <img src='${eachGadget.gadgetImageUrl}'>
                     <div><button type='button' id='add-cart' onclick='addToCart(${eachGadget.id})'>Add to Cart</button></div>
@@ -24,29 +23,37 @@ function displayGadgets(){
                     <p>Gadget Cost: Rs.${eachGadget.gadgetCost}</p>
                     
                 </div>`;
-    }
-    document.getElementById("flex-container").innerHTML = data;
+      }
+      document.getElementById("flex-container").innerHTML = data;
+    })
+    .catch((error) => console.log(error));
+  // and call this function in displayGadgets()
 }
 
+function displayGadgets() {
+  loadGadgets();
 
-function addToCart(gadgetId){
-    //alert(gadgetId);
-    // traverse the allGadgets array and findout the gadget with the id gadgetId
-    for(let eachGadget of allGadgets){
-        if(eachGadget.id == gadgetId){
-            // then add this gadget object to the array of allCartGadgets
-            allCartGadgets.push(eachGadget);
-            break;
-        }
-    }
-    //console.log(allCartGadgets);
-    // then display the allCartGadgets array in a table
-    displayCart();
+  // traverse through the allGadgets array and display them
 }
 
-function displayCart(){
-    let totalCost = 0;
-    let data = `<h3 class="cart-head">CART ITEMS</h3>
+function addToCart(gadgetId) {
+  //alert(gadgetId);
+  // traverse the allGadgets array and findout the gadget with the id gadgetId
+  for (let eachGadget of allGadgets) {
+    if (eachGadget.id == gadgetId) {
+      // then add this gadget object to the array of allCartGadgets
+      allCartGadgets.push(eachGadget);
+      break;
+    }
+  }
+  //console.log(allCartGadgets);
+  // then display the allCartGadgets array in a table
+  displayCart();
+}
+
+function displayCart() {
+  let totalCost = 0;
+  let data = `<h3 class="cart-head">CART ITEMS</h3>
                 <table>
                     <thead>
                         <tr>
@@ -56,10 +63,10 @@ function displayCart(){
                             <th></th>
                         </tr>
                     <thead>
-                    <tbody>`
-    for(let eachCartItem of allCartGadgets){
-        totalCost += eachCartItem.gadgetCost;
-        data += `<tr>
+                    <tbody>`;
+  for (let eachCartItem of allCartGadgets) {
+    totalCost += eachCartItem.gadgetCost;
+    data += `<tr>
                     <td>${eachCartItem.id}</td>
                     <td>${eachCartItem.gadgetName}</td>
                     <td>Rs.${eachCartItem.gadgetCost}</td>
@@ -67,19 +74,18 @@ function displayCart(){
                     delete
                     </span></button></td>
                 </tr>`;
-    }
-    data += `</tbody></table>`;
-    data += `<h4>TOTAL COST : Rs. ${totalCost}</h4>`;
-    document.getElementById("div1").innerHTML = data;
+  }
+  data += `</tbody></table>`;
+  data += `<h4>TOTAL COST : Rs. ${totalCost}</h4>`;
+  document.getElementById("div1").innerHTML = data;
 }
 
-function removeFromCart(gadgetId){
-    for(let i=0;i<allCartGadgets.length;i++){
-        if(allCartGadgets[i].id == gadgetId){
-            allCartGadgets.splice(i,1);
-            break;
-        }
+function removeFromCart(gadgetId) {
+  for (let i = 0; i < allCartGadgets.length; i++) {
+    if (allCartGadgets[i].id == gadgetId) {
+      allCartGadgets.splice(i, 1);
+      break;
     }
-    displayCart();
+  }
+  displayCart();
 }
-   
